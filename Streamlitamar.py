2300011cf3521ef74c691bc46bec0dac1d4a0983
amarshinde10 <97755132+amarshinde10@users.noreply.github.com>
2022-12-10 00:25:06 +0530

    Update Streamlitamar.py

--- Streamlitamar.py
+++ Streamlitamar.py
@@ -1,2 +1,2 @@
 import streamlit as st
-st.write("Hello ,let's learn how to build a streamlit app together")
+st.write("Hello ,let's learn how to build a streamlit app together , Love you Trisha and Vanita")
